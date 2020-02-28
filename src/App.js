b939756cdb763d1a667d8f02b68c1c443d457da2
Andrey Martemyanov <marty105k@gmail.com>
2020-02-28 15:34:36 +0300

    neuro-react done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {users:[]};
+  }
+
+  componentDidMount(){
+    fetch('https://reqres.in/api/users')
+      .then(response => response.json())
+      .then(json => this.setState({users: json.data}));
+  }
+
+  render(){
+    return (
+      <div className='wrapper'>
+        <h1 className='title'>users</h1>
+        <ul className='list'>
+          {
+            this.state.users.length == 0 ? '' : this.state.users.map(user => (
+                <li className='item' key={user.id}>
+                  <img className='image' src={user.avatar}/>
+                  <p className='email'>{user.email}</p>
+                  <p className='name'>{user.first_name} {user.last_name}</p>
+                </li>
+              ))
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
